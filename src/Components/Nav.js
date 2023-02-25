17736a6bb8c02fc585dcdc49fa296f1a810b5489
Killua <thomas.nguyen3@mail.mcgill.ca>
2023-02-25 15:10:38 -0500

    fixed path

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,7 +10,7 @@ const Nav = () => {
 
     return (
         <nav className="nav1">
-            <Link style={linkStyle} to={'/'}><h3>Nvidia</h3></Link>
+            <Link style={linkStyle} to={'/NVIDIA-Store'}><h3>Nvidia</h3></Link>
             <ul className="nav-links">
                 <li><Link style={linkStyle} to={'/Shop'}>Shop</Link></li>
                 <li>Cart</li>
--- src/Components/Nav2.js
+++ src/Components/Nav2.js
@@ -10,7 +10,7 @@ const Nav2 = () => {
 
     return (
         <nav className="nav2">
-            <Link style={linkStyle} to={'/'}><h3>Nvidia</h3></Link>
+            <Link style={linkStyle} to={'/NVIDIA-Store'}><h3>Nvidia</h3></Link>
             <ul className="nav-links">
                 <li><Link style={linkStyle} to={'/Shop'}>Shop</Link></li>
                 <li>Cart</li>
